eda05fb427315f2550d87e66552d93ee4f12b106
james <jameshaller27@gmail.com>
2019-08-01 22:30:55 -0700

    Add static list of recipes

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,5 +1,17 @@
 var app = angular.module('recipeApp', []);
 
+class Recipe {
+    constructor(name, description, tags, ingredients, prep, steps, notes) {
+        this.Name = name;
+        this.Description = description;
+        this.Tags = tags;
+        this.Ingredients = ingredients;
+        this.Prep = prep;
+        this.Steps = steps;
+        this.Notes = notes;
+    }
+}
+
 app.controller('recipeCtrl', function ($scope) {
     $scope.names = [
         {first: 'James', last: 'Haller'},
@@ -8,6 +20,12 @@ app.controller('recipeCtrl', function ($scope) {
         {first: 'Jon', last: 'Snow'},
         {first: 'Arya', last: 'Nobody'},
         {first: 'Rob', last: 'Stark'}
+    ];
+
+    $scope.recipes = [
+        new Recipe("Milk in a cup", "", ["Easy", "Vegetarian"], ["Milk"], [], ["Pour milk into cup"], "Cold milk is best!"),
+        new Recipe("Chicken Nuggets", "The cheap and dirty way", ["Chicken", "Easy"], ["Tyson's Fun Nuggets"], [], ["Place 5 chicken nuggets on a plate", "Cook for 1:30", "(Optional) serve with Kraft Chipotle Aioli sauce", "Enjoy!"], ""),
+        new Recipe("Buffalo Chicken Mac & Cheese", "From the kitchen of Jessica Ketchum", ["Chicken", "Pasta"], ["Annie's Cheddar Mac & Cheese", "Tyson Buffalo Chicken Tenders"], ["Preheat oven to 400F"], ["Bake chicken tenders for 15-20 minutes", "Follow directions on Mac & Cheese box", "Slice chicken", "Stir chicken into mac & cheese"], "Best served warm!")
     ];
 });
 
