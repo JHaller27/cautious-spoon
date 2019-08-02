a67330ffea08502587e9ae2f4c04c641f2c39ed8
james <jameshaller27@gmail.com>
2019-08-02 13:36:52 -0700

    Add theme selection box

--- includes/scripts.js
+++ includes/scripts.js
@@ -13,9 +13,12 @@ class Recipe {
 }
 
 app.controller('recipeCtrl', function ($scope) {
+    $scope.themes = ['water', 'earth', 'fire', 'air', 'magic', 'light', 'dark'];
     $scope.recipes = [
         new Recipe("Milk in a cup", "", ["Easy", "Vegetarian"], ["Milk"], [], ["Pour milk into cup"], "Cold milk is best!"),
         new Recipe("Chicken Nuggets", "The cheap and dirty way", ["Chicken", "Easy"], ["Tyson's Fun Nuggets"], [], ["Place 5 chicken nuggets on a plate", "Cook for 1:30", "(Optional) serve with Kraft Chipotle Aioli sauce", "Enjoy!"], ""),
         new Recipe("Buffalo Chicken Mac & Cheese", "From the kitchen of Jessica Ketchum", ["Chicken", "Pasta"], ["Annie's Cheddar Mac & Cheese", "Tyson Buffalo Chicken Tenders"], ["Preheat oven to 400F"], ["Bake chicken tenders for 15-20 minutes", "Follow directions on Mac & Cheese box", "Slice chicken", "Stir chicken into mac & cheese"], "Best served warm!")
     ];
+
+    $scope.currentTheme = $scope.themes[0];
 });
