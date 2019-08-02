652032c9c0e99abe9f02efea2c057b16d2e2985f
james <jameshaller27@gmail.com>
2019-08-02 13:33:08 -0700

    Remove unused JS code

--- includes/scripts.js
+++ includes/scripts.js
@@ -13,30 +13,9 @@ class Recipe {
 }
 
 app.controller('recipeCtrl', function ($scope) {
-    $scope.names = [
-        {first: 'James', last: 'Haller'},
-        {first: 'Jessica', last: 'Ketchum'},
-        {first: 'Brian', last: 'Hanby'},
-        {first: 'Jon', last: 'Snow'},
-        {first: 'Arya', last: 'Nobody'},
-        {first: 'Rob', last: 'Stark'}
-    ];
-
     $scope.recipes = [
         new Recipe("Milk in a cup", "", ["Easy", "Vegetarian"], ["Milk"], [], ["Pour milk into cup"], "Cold milk is best!"),
         new Recipe("Chicken Nuggets", "The cheap and dirty way", ["Chicken", "Easy"], ["Tyson's Fun Nuggets"], [], ["Place 5 chicken nuggets on a plate", "Cook for 1:30", "(Optional) serve with Kraft Chipotle Aioli sauce", "Enjoy!"], ""),
         new Recipe("Buffalo Chicken Mac & Cheese", "From the kitchen of Jessica Ketchum", ["Chicken", "Pasta"], ["Annie's Cheddar Mac & Cheese", "Tyson Buffalo Chicken Tenders"], ["Preheat oven to 400F"], ["Bake chicken tenders for 15-20 minutes", "Follow directions on Mac & Cheese box", "Slice chicken", "Stir chicken into mac & cheese"], "Best served warm!")
     ];
-});
-
-app.filter('lnameFilter', function () {
-    return function (people, scope) {
-        let result = [];
-        people.forEach(person => {
-            if (scope.name === undefined || scope.name === '' || person.last.toLowerCase().indexOf(scope.name.toLowerCase()) >= 0) {
-                result.push(person);
-            }
-        });
-        return result;
-    };
 });
