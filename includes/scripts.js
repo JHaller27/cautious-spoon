78f0218b24947ae0ab556fed499f82a0e69b32f2
james <jameshaller27@gmail.com>
2019-08-03 01:46:05 -0700

    Generate list of tags for filtering

--- includes/scripts.js
+++ includes/scripts.js
@@ -34,14 +34,52 @@ function strContains(superstring, substring, caseSensitive=false) {
 app.controller('recipeCtrl', function ($scope) {
     $scope.themes = ['light', 'dark', 'water', 'earth', 'fire', 'air', 'magic'];
     $scope.recipes = [
-        new Recipe("Milk in a cup", "", ["Easy", "Vegetarian"], ["Milk"], [], ["Pour milk into cup"], "Cold milk is best!"),
-        new Recipe("Chicken Nuggets", "The cheap and dirty way", ["Chicken", "Easy"], ["Tyson's Fun Nuggets"], [], ["Place 5 chicken nuggets on a plate", "Cook for 1:30", "(Optional) serve with Kraft Chipotle Aioli sauce", "Enjoy!"], ""),
-        new Recipe("Buffalo Chicken Mac & Cheese", "From the kitchen of Jessica Ketchum", ["Chicken", "Pasta"], ["Annie's Cheddar Mac & Cheese", "Tyson Buffalo Chicken Tenders"], ["Preheat oven to 400F"], ["Bake chicken tenders for 15-20 minutes", "Follow directions on Mac & Cheese box", "Slice chicken", "Stir chicken into mac & cheese"], "Best served warm!"),
-        new Recipe("Cheese Burgers", "Classic American", ["Beef", "Grill"], ["Burger buns", "Ground beef (patties)", "Condiments"], ["Light the grill OR Pre-heat stove"], ["Place patties on grill/stove", "Wait until browned", "Flip", "Wait until browned on both sides", "Place sliced cheese on patties", "Cook for no longer than 1 more minute"], "Easiest on grill")
+        new Recipe("Milk in a cup", "", ["Difficulty:Easy", "Meat:None"], ["Milk"], [], ["Pour milk into cup"], "Cold milk is best!"),
+        new Recipe("Chicken Nuggets", "The cheap and dirty way", ["Meat:Chicken", "Difficulty:Easy"], ["Tyson's Fun Nuggets"], [], ["Place 5 chicken nuggets on a plate", "Cook for 1:30", "(Optional) serve with Kraft Chipotle Aioli sauce", "Enjoy!"], ""),
+        new Recipe("Buffalo Chicken Mac & Cheese", "From the kitchen of Jessica Ketchum", ["Meat:Chicken", "Pasta"], ["Annie's Cheddar Mac & Cheese", "Tyson Buffalo Chicken Tenders"], ["Preheat oven to 400F"], ["Bake chicken tenders for 15-20 minutes", "Follow directions on Mac & Cheese box", "Slice chicken", "Stir chicken into mac & cheese"], "Best served warm!"),
+        new Recipe("Cheese Burgers", "Classic American", ["Meat:Beef", "Grill"], ["Burger buns", "Ground beef (patties)", "Condiments"], ["Light the grill OR Pre-heat stove"], ["Place patties on grill/stove", "Wait until browned", "Flip", "Wait until browned on both sides", "Place sliced cheese on patties", "Cook for no longer than 1 more minute"], "Easiest on grill")
     ];
 
     $scope.storeTheme = function (newTheme) {
         localStorage.setItem(STORED_THEME, newTheme);
+    }
+
+    $scope._cachedTags = null;
+    $scope.getTags = function () {
+        if ($scope._cachedTags === null) {
+            // Generate new tags
+            $scope._cachedTags = {
+                Categories: ['Custom'],
+                Custom: []
+            };
+
+            $scope.recipes.forEach(recipe => {
+                recipe.Tags.forEach(tag => {
+                    let categorySplit = tag.indexOf(':');
+                    if (categorySplit < 0) {
+                        $scope._cachedTags.Custom.push(tag);
+                    }
+                    else if (categorySplit === 0) {
+                        $scope._cachedTags.Custom.push(tag.substr(categorySplit, tag.indexOf(':')));
+                    }
+                    else {
+                        let category = tag.substr(0, tag.indexOf(':'));
+                        let tagName = tag.substr(tag.indexOf(':')+1);
+                        if ($scope._cachedTags[category] === undefined) {
+                            $scope._cachedTags[category] = [tagName];
+                            $scope._cachedTags.Categories.push(category);
+                        }
+                        else if (!$scope._cachedTags[category].includes(tagName)) {
+                            $scope._cachedTags[category].push(tagName);
+                        }
+                    }
+                });
+            });
+
+            console.log($scope._cachedTags);
+        }
+
+        return $scope._cachedTags;
     }
 
     function init() {
